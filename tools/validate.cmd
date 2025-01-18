e2de5486596d867dd75fb514a12c7a5bd2cb28a4
Fernando-A-Rocha <frocha2001@gmail.com>
2025-01-18 15:28:08 +0000

    Update validator

--- tools/validate.cmd
+++ tools/validate.cmd
@@ -1,5 +1,12 @@
 @echo off
 
+.\tools\yajsv.exe -s schemas/navigation.yaml "wiki/navigation.yaml"
+.\tools\yajsv.exe -s schemas/categories.yaml "wiki/categories.yaml"
+
 for /r "functions" %%f in (*.yaml) do (
     .\tools\yajsv.exe -s schemas/function.yaml "%%f"
+)
+
+for /r "articles" %%f in (*.yaml) do (
+    .\tools\yajsv.exe -s schemas/article.yaml "%%f"
 )
