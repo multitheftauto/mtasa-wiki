96e954808e9aa94d305c1d923095879bac60d22a
Fernando-A-Rocha <frocha2001@gmail.com>
2025-01-18 15:23:08 +0000

    Update validator

--- tools/validate.sh
+++ tools/validate.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+tools/yajsv -s schemas/structure.yaml wiki/structure.yaml
+tools/yajsv -s schemas/categories.yaml wiki/categories.yaml
+
 find 'functions/' -name '*.yaml' -type f -print0 | xargs -0 -I {} tools/yajsv -s schemas/function.yaml {}
+find 'articles/' -name '*.yaml' -type f -print0 | xargs -0 -I {} tools/yajsv -s schemas/article.yaml {}
